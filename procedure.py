381fc977ba77fa9af90cb98e7c8aad47210540a6
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2023-03-02 14:11:36 -0700

    Update procedure.py

--- procedure.py
+++ procedure.py
@@ -1,13 +1,13 @@
 import random
 mapGrid = [[0 for i in range(10)] for j in range(10)]
-x = random.randrange(0,9) 
-y = random.randrange(0,9)
+x = random.randint(0,9) 
+y = random.randint(0,9)
 
+rooms = 0
 
+while rooms < 10:
 
-for i in range (10):
-
-    direction  = random.randrange(0,3)
+    direction  = random.randint(0,3)
 
     if direction == 0:
         direction = "up"
@@ -25,21 +25,25 @@ for i in range (10):
         if y > 0 and mapGrid[x][y-1] == 0:
             mapGrid[x][y-1] = 2
             y-=1
+            rooms +=1
         
     elif direction == "down":
         if y < 9 and mapGrid[x][y+1] == 0:
             mapGrid[x][y+1] = 3
             y+=1
+            rooms +=1
 
     elif direction == "right":
         if x > 0 and mapGrid[x-1][y] == 0:
             mapGrid[x-1][y] = 4
             x-=1
+            rooms +=1
 
     else:
         if x < 9 and mapGrid[x+1][y] == 0:
             mapGrid[x+1][y] = 5
             x+=1
+            rooms +=1
 
 mapGrid[x][y] = 1
 for row in mapGrid:
